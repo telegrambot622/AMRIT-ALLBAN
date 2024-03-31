59592d983aa890b7183e885025cd5cee0b2f84a6
telegrambot622 <158012060+telegrambot622@users.noreply.github.com>
2024-03-31 06:13:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 class Config:
     API_ID=22981583
     API_HASH="c02ca0834d5d92ffb31fea2b50060195"
-    TOKEN="7156025226:AAFaTvFJwfkHLGcP2JZqY6GbfnUfajkQY5o"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6752444356").split(" "))
-    START_IMG="https://te.legra.ph/file/735920504b44b3565242a.jpg"
+    TOKEN="6904405657:AAEQLaE8KjZfuISuw9zuotPzLwrNzFJbXR8"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "7110021757").split(" "))
+    START_IMG="https://telegra.ph/file/759e5775cbcfd05bc5aa0.jpg"
     BOT_ID=7156025226
-    BOT_USERNAME="SOMEONE47BOT"
+    BOT_USERNAME="ANIME_BAN_BOT"
     BOT_NAME="SOMEONE47BOT"
